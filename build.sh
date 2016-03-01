d299a7ba1b25856b1c2c6febb5f22d5363de77d5
Micah Young <micah@young.io>
2016-03-01 13:02:44 -0500

    Add size option
    Move up fetching to fail fast

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -ex
 
 outdir=$1
+size_mbytes=${2:-2000} #2GB default
 tmpdir=tmp/
 latest_url=http://archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz
 sector_bytes=512
@@ -10,12 +11,18 @@ mbr_sectors=2048
 boot_start_sectors=2048
 boot_sectors=204800
 root_start_sectors=206848
-root_sectors=3889152
+#root_sectors=3889152
+root_sectors=$((($size_mbytes * 1000000 / $sector_bytes) - $boot_start_sectors))
 boot_start_bytes=$(($boot_start_sectors * $sector_bytes))
 root_start_bytes=$(($root_start_sectors * $sector_bytes))
 
 if [ ! -d "$outdir" ]; then 
-  echo "outdir required"
+  echo "usage: build.sh out_dir [size_mbs]"
+  exit 1
+fi
+
+if [ $size_mbytes -lt 2000 ]; then 
+  echo "Size $size_mbytes must be greater than 2,000MB"
   exit 1
 fi
 
@@ -24,6 +31,8 @@ if [ ! -d "$tmpdir" ]; then
 fi
 
 pushd $tmpdir/
+	wget --timestamping $latest_url -O alarpmi.tgz
+
 	dd if=/dev/zero of=mbr.img  bs=$sector_bytes count=$mbr_sectors
 	dd if=/dev/zero of=boot.img bs=$sector_bytes count=$boot_sectors
 	dd if=/dev/zero of=root.img bs=$sector_bytes count=$root_sectors
@@ -48,8 +57,7 @@ EOF
 	mount -o loop,offset=$boot_start_bytes archlinux.img ./boot
 	mount -o loop,offset=$root_start_bytes archlinux.img ./root
 
-	wget --timestamping $latest_url
-	tar -xzpf ArchLinuxARM-rpi-latest.tar.gz -C ./root
+	tar -xzpf alarpmi.tgz -C ./root
 
 	mv ./root/boot/* ./boot
 
